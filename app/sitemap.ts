73f7c89c4b723afb798f2b42557ee1fb9cf2761e
Prakash-56 <prakash.connect06@gmail.com>
2025-12-03 11:30:33 +0530

    Added sitemap and robots

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://orbitalpaintsolutionc.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://orbitalpaintsolutionc.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://orbitalpaintsolutionc.com/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://orbitalpaintsolutionc.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
